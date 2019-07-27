eac03c70aae327e82b29b34bc0f35e3ddaca52f2
Suman Koirala <sumanrakoirala@gmail.com>
2019-07-27 11:02:34 -0500

    api path added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'urls/top'
+      get 'urls/url'
+      get 'urls/show'
+    end
+  end
   root to: 'pages#index'
   get 'pages/index'
 end
