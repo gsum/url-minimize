4b3935e9a783822e4e32a69d7158cdda3c0765a2
Suman Koirala <sumanrakoirala@gmail.com>
2019-07-27 14:06:55 -0500

    better-errors and awesome-print added for debuginh

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,10 @@ class PagesController < ApplicationController
   end
 
   def show
+    short_url_object = ShortUrl.find_by_base_url(params[:unmatched_route])
+    if short_url_object.present?
+      redirect_to "http://minecraft.com", status: 301
+
+    end
   end
 end
