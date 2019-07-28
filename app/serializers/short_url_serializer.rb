af02232c49ed46692b511b5dd81ca63dc848fb08
Suman Koirala <sumanrakoirala@gmail.com>
2019-07-27 23:41:41 -0500

    heroku host issue fix

--- app/serializers/short_url_serializer.rb
+++ app/serializers/short_url_serializer.rb
@@ -2,6 +2,6 @@ class ShortUrlSerializer < ActiveModel::Serializer
   attributes :short_url, :original_url, :hit_count
 
   def short_url
-    "#{ENV["HOST"]}/#{object.base_url}"
+    "#{ENV["HOST-URL"]}/#{object.base_url}"
   end
 end
